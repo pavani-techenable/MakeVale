a5f923e0f95c516906049f2d7efc17a8da75fdc1
pavani-techenable <pavani@techenable.io>
2026-03-12 18:11:21 +0530

    all code

---
+++ SciNoteIntegrator_11072024_ScinoteTOStaging (2)/SciNoteIntegrator_11072024_ScinoteTOStaging/SciNoteIntegrator/SciNoteDIApi/globalVar.vb
@@ -0,0 +1,367 @@
+﻿Imports System.Data.SqlClient
+Imports System.IO
+
+Module globalVar
+    Public objCompany As SAPbobsCOM.Company
+    Public woo_order_url
+    Public woo_uid, sapExpnsCode
+    Public server, Database, sapUser, sapPassword, attPath, attachPathLog As String
+    Public dbName As String
+    Public dbPassword As String
+    Public dbUser As String
+    Public sapWHSCODE As String
+    Public sapEXPTAXCODE As String
+    Public SQLQueryForCustMapping As String
+    'Public TWSAPDB = "TWSAPDB"
+
+    Public Sub writeLog(ByVal msgString As String, ByVal boolSuccess As Boolean)
+        Dim today As String = Date.Now.ToString("ddMMMyyyy")
+        ' Dim strFile As String = "ASTL_Doff_Log" + today + ".txt"
+        Dim SPath As String = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)
+        'Dim strFile As String = SPath + "\MakeValeSAPDIAPI_Log" + today + ".txt"
+        ' Dim strFile As String = "P:\SAPWOOINT\Sci\Log\SciNoteSAPDIAPI_Log" + today + ".txt"
+        Dim strFile As String = SPath + "\SciNoteSAPDIAPI_Log" + today + ".txt"
+        Dim fileExists As Boolean = File.Exists(strFile)
+        If boolSuccess Then
+            msgString = "Success: " & msgString
+        Else
+            msgString = "Error: " & msgString
+        End If
+        Using sw As New StreamWriter(File.Open(strFile, FileMode.Append))
+            sw.WriteLine(
+                IIf(fileExists,
+                     DateTime.Now & "-" & msgString,
+                  DateTime.Now & "-" & msgString))
+        End Using
+    End Sub
+
+
+    Public Sub writeLogAttach(ByVal msgString As String, ByVal boolSuccess As Boolean)
+        Dim today As String = Date.Now.ToString("ddMMMyyyy")
+
+        'Dim SPath As String = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)
+
+        Dim strFile As String = String.Format(attachPathLog, today + ".txt")
+
+        Dim fileExists As Boolean = File.Exists(strFile)
+        If boolSuccess Then
+            msgString = "Success: " & msgString
+        Else
+            msgString = "Error: " & msgString
+        End If
+        Using sw As New StreamWriter(File.Open(strFile, FileMode.Append))
+            sw.WriteLine(
+                IIf(fileExists,
+                     DateTime.Now & "-" & msgString,
+                  DateTime.Now & "-" & msgString))
+        End Using
+    End Sub
+    Public Sub ExcuteNonQuery(ByVal cmdString As String)
+        Try
+            Dim con As New SqlConnection
+            Dim cmd As New SqlCommand
+            'Dim server As String = server
+            'Dim dbName As String = dbName
+            'Dim dbPassword As String = dbPassword
+            'Dim dbUser As String = objMain.objCompany.DbUserName
+            Try
+                con.ConnectionString = "server=" + server + ";user=sa;password=" + dbPassword + ";database=" + dbName
+                '  con.ConnectionString = "server=" + "SERVER\SAPSERVER" + ";user=sa;password=abc@1234;database=" + dbName
+                con.Open()
+                cmd.Connection = con
+                cmd.CommandText = cmdString
+                cmd.ExecuteNonQuery()
+
+            Catch ex As Exception
+                writeLog(ex.Message.ToString, False)
+                ' MessageBox.Show("Error while inserting record on table..." & ex.Message, "Insert Records")
+            Finally
+                con.Close()
+            End Try
+        Catch ex As Exception
+
+        End Try
+    End Sub
+    Public Sub ExcuteNonQuerySciNote(ByVal cmdString As String, Optional writelog_ As Boolean = False)
+        Try
+            Dim con As New SqlConnection
+            Dim cmd As New SqlCommand
+            'Dim server As String = server
+            'Dim dbName As String = dbName
+            'Dim dbPassword As String = dbPassword
+            'Dim dbUser As String = objMain.objCompany.DbUserName
+            Try
+                con.ConnectionString = "server=" + server + ";user=sa;password=" + dbPassword + ";database=" + Database
+                '  con.ConnectionString = "server=" + "SERVER\SAPSERVER" + ";user=sa;password=abc@1234;database=" + dbName
+                con.Open()
+                cmd.Connection = con
+                cmd.CommandText = cmdString
+                cmd.ExecuteNonQuery()
+
+            Catch ex As Exception
+                If writelog_ = True Then
+                    writeLog(ex.Message.ToString, False)
+                End If
+
+                ' MessageBox.Show("Error while inserting record on table..." & ex.Message, "Insert Records")
+            Finally
+                con.Close()
+            End Try
+        Catch ex As Exception
+
+        End Try
+    End Sub
+    Public Function getDataTable(ByVal sqlQuery As String)
+        Dim oDT As New DataTable
+        Dim Connection = "server=" + server + ";user=sa;password=" + dbPassword + ";database=" + Database
+        Dim myConnection As New SqlConnection(Connection)
+        Try
+            myConnection.Open()
+            Dim objda As New SqlDataAdapter(sqlQuery, myConnection)
+            Dim dt As New DataTable()
+
+            objda.Fill(dt)
+            myConnection.Close()
+            oDT = dt
+        Catch ex As Exception
+            myConnection.Close()
+            '   psReturn = ex.Message
+        End Try
+        Return oDT
+    End Function
+
+    Public Function getDataTableSAP(ByVal sqlQuery As String)
+        Dim oDT As New DataTable
+        Dim Connection = "server=" + server + ";user=sa;password=" + dbPassword + ";database=" + dbName
+        Dim myConnection As New SqlConnection(Connection)
+        Try
+            myConnection.Open()
+            Dim objda As New SqlDataAdapter(sqlQuery, myConnection)
+            Dim dt As New DataTable()
+
+            objda.Fill(dt)
+            myConnection.Close()
+            oDT = dt
+        Catch ex As Exception
+            myConnection.Close()
+            '   psReturn = ex.Message
+        End Try
+        Return oDT
+    End Function
+
+    Public Sub Main()
+        Try
+            writeLog(String.Format("main started"), True)
+
+            'writeLog("Main started", False)
+            Dim objDiOperations = New clsDIoperations
+            objDiOperations.readFromXML()
+            objDiOperations.createConnectionToCompany()
+            objDiOperations.createSciNoteUDF()
+            writeLog(String.Format("Connected to company"), True)
+            '' Dim GB_CardCode As String
+            ' Application.Exit()
+            'objDiOperations.createActivity()
+            'Application.Exit()
+            'Dim dt_projectData As New DataTable
+            'dt_projectData = getDataTable("Select * from ServiceContractData")
+            'For Each row As DataRow In dt_projectData.Rows
+            '    Try
+            '        Dim DocEntry As Integer = 0
+            '        Dim errors_ As New List(Of String)
+            '        Dim CardCode As String = row("CardCode")
+            '        Dim projectId As String = row("projectId")
+            '        Dim projectDescription As String = row("projectDescription")
+            '        Dim startDate As DateTime = row("start_date")
+            '        objDiOperations.createServiceContract(CardCode, projectId, projectDescription, startDate, errors_, False, DocEntry)
+            '        If errors_.Count = 0 And DocEntry <> 0 Then
+            '            Dim updateProject = String.Format("Update projects set Sync = 'Yes',DocEntry = '{0}' where s_id = '{1}'", DocEntry.ToString, projectId)
+            '            ExcuteNonQuerySciNote(updateProject)
+            '        End If
+            '    Catch ex As Exception
+
+            '    End Try
+            'Next
+
+            Dim dt_expData As New DataTable
+            dt_expData = getDataTable("Select * from experimentsData")
+            For Each row As DataRow In dt_expData.Rows
+                Try
+                    Dim DocEntry As Integer = 0
+                    Dim errors_ As New List(Of String)
+                    Dim CardCode As String = row("CardCode")
+                    Dim expId As String = row("s_id")
+                    Dim pId As String = row("p_id")
+                    Dim name As String = row("name")
+                    Dim description As String = row("description")
+                    Dim ContractId As String = row("ContractId")
+                    Dim created_at As DateTime = row("created_at")
+                    objDiOperations.createServiceCalls(pId, expId, CardCode, name, description, ContractId, created_at, errors_, False, DocEntry)
+                    If errors_.Count = 0 And DocEntry <> 0 Then
+                        Dim updateProject = String.Format("Update experiments set Sync = 'Yes',DocEntry = '{0}' where s_id = '{1}'", DocEntry.ToString, expId)
+                        ExcuteNonQuerySciNote(updateProject)
+                    End If
+                Catch ex As Exception
+
+                End Try
+            Next
+            writeLog(String.Format("Activity Started"), True)
+            'Create Activity 
+            Dim strtaskdata_ As String = "Select t0.*,t1.DocEntry,t2.CardCode   from tasks t0 inner join experiments t1 on t1.t_id = t0.t_id and t0.p_id =t1.p_id and t0.e_id =t1.s_id inner join teams t2 on t2.s_id =t0.t_id   where 
+t1.DocEntry is not null  and t0.DocEntry is Null"
+
+            Dim strtaskdata As String = "Select  Distinct t0.t_id , t0.p_id, t0.e_id   from tasks t0 inner join experiments t1 on t1.t_id = t0.t_id and t0.p_id =t1.p_id and t0.e_id =t1.s_id inner join teams t2 on t2.s_id =t0.t_id   where 
+t1.DocEntry is not null  and t0.DocEntry is Null"
+            Dim dt_taskData As New DataTable
+            dt_taskData = getDataTable(strtaskdata)
+            For Each rw As DataRow In dt_taskData.Rows
+
+                Try
+                    Dim t_id = rw("t_id")
+                    Dim p_id = rw("p_id")
+                    Dim e_id = rw("e_id")
+                    writeLog(String.Format("Started loop in activity creation t_id = {0} , p_id = {1} ,e_id = {2} ", t_id, p_id, e_id), True)
+                    Dim strtaskdata_1 As String = String.Format("Select  Distinct t0.t_id , t0.p_id, t0.e_id   from tasks t0 inner join experiments t1 on t1.t_id = t0.t_id and t0.p_id =t1.p_id and t0.e_id =t1.s_id inner join teams t2 on t2.s_id =t0.t_id   where 
+t1.DocEntry is not null  and t0.DocEntry is Null and t0.t_id ={0} and t0.p_id={1} and t0.e_id={2}", t_id, p_id, e_id)
+                    Dim dt_taskData_ As New DataTable
+                    dt_taskData_ = getDataTable(strtaskdata_1)
+                    objCompany.StartTransaction()
+                    For Each row As DataRow In dt_taskData_.Rows
+                        Try
+                            'writeLog(String.Format("Started loop in activity creation activity = {0}", row("s_id")), True)
+                            writeLog(String.Format("Started loop in activity creation activity = {0}", row("e_id")), True)
+
+
+                            Dim DocEntry As Integer = 0
+                            Dim errors_ As New List(Of String)
+                            Dim name As String = ""
+                            If Not IsDBNull(Row("name")) Then
+                                name = Row("name")
+                            Else
+                                name = ""
+                            End If
+                            Dim taskId As String = ""
+                            'If Not IsDBNull(Row("s_id")) Then
+                            '    taskId = Row("s_id")
+                            'End If
+
+                            If Not IsDBNull(row("e_id")) Then
+                                taskId = row("e_id")
+                            End If
+
+                            Dim pId As String = ""
+                            If Not IsDBNull(Row("p_id")) Then
+                                pId = Row("p_id")
+
+
+                            End If
+                            Dim eId As String = ""
+                            If Not IsDBNull(Row("e_id")) Then
+                                eId = Row("e_id")
+
+
+                            End If
+                            Dim Decription As String = ""
+                            If Not IsDBNull(Row("description")) Then
+                                Decription = Row("description")
+
+
+                            End If
+                            Dim cardCode As String = ""
+                            If Not IsDBNull(Row("CardCode")) Then
+                                cardCode = Row("CardCode")
+
+
+                            End If
+                            Dim created_at As Date
+                            'writeLog(String.Format("Sap loop in activity creation {0}", row("s_id")), True)
+                            writeLog(String.Format("Sap loop in activity creation {0}", row("e_id")), True)
+                            writeLog("M Task:" + taskId + " " + DateTime.Today.ToString, True)
+                            writeLog("M Experiment Task:" + eId + " " + DateTime.Today.ToString, True)
+                            writeLog("M Pid Task:" + pId + " " + DateTime.Today.ToString, True)
+                            writeLog("M Description: " + Decription + " " + DateTime.Today.ToString, True)
+                            Try
+                                created_at = Row("created_at")
+                            Catch ex As Exception
+
+                            End Try
+                            objDiOperations.createActivity(eId, pId, cardCode, name, taskId, errors_, DocEntry, Decription, created_at)
+                            If errors_.Count = 0 And DocEntry <> 0 Then
+                                Dim updateProject = String.Format("Update tasks set DocEntry = '{0}' where s_id = '{1}'", DocEntry.ToString, taskId)
+                                ExcuteNonQuerySciNote(updateProject)
+                            End If
+                            If Not (objCompany.InTransaction()) Then
+                                objCompany.EndTransaction(SAPbobsCOM.BoWfTransOpt.wf_RollBack)
+                            End If
+                        Catch ex As Exception
+                            'writeLog(String.Format("Exception loop in activity creation s_id = {0} " + ex.Message, row("s_id")), True)
+                            writeLog(String.Format("Exception loop in activity creation s_id = {0} " + ex.Message, row("e_id")), True)
+                        End Try
+                    Next
+                    objCompany.EndTransaction(SAPbobsCOM.BoWfTransOpt.wf_Commit)
+                Catch ex As Exception
+
+                End Try
+
+            Next
+            Try
+                'Update Activity 
+                Dim taskdataUpdate As String = "Select T0.id,T0.t_id,T0.p_id,T0.e_id,T0.s_id,T0.name,T0.started_on,T0.due_date,dbo.[udf_StripHTML](T0.description)description,T0.state,T0.archived,T0.status_id,T0.status_name,T0.prev_status_id,T0.prev_status_name,T0.next_status_id,T0.next_status_name,T0.x,T0.y,T0.created_at,T0.updated_at,T0.Sync,T0.DocEntry,T0.updatedes  from tasks t0 inner join experiments t1 on t1.t_id = t0.t_id and t0.p_id =t1.p_id and t0.e_id =t1.s_id inner join teams t2 on t2.s_id =t0.t_id   where 
+t1.DocEntry is not null  and t0.DocEntry is not Null and T0.updatedes ='Yes'"
+                Dim dt_taskDataup As New DataTable
+                dt_taskDataup = getDataTable(taskdataUpdate)
+                For Each row As DataRow In dt_taskDataup.Rows
+                    Try
+                        Dim DocEntry As Integer = row("DocEntry")
+                        Dim errors_ As New List(Of String)
+                        Dim name As String = row("name")
+                        Dim taskId As String = row("s_id")
+                        Dim Decription As String = row("description")
+
+                        objDiOperations.updateActivity(errors_, DocEntry, Decription)
+                        If errors_.Count = 0 And DocEntry <> 0 Then
+                            Dim updateProject = String.Format("Update tasks set updatedes = 'No' where DocEntry = '{0}'", DocEntry.ToString, taskId)
+                            ExcuteNonQuerySciNote(updateProject)
+                        End If
+                    Catch ex As Exception
+
+                    End Try
+                Next
+            Catch ex As Exception
+
+            End Try
+            'Link Activity to Service Call
+            Dim strlinkSer As String = "Select t0.s_id ,t0.DocEntry ActivityId,t1 .DocEntry ServiceId  from tasks t0 inner join experiments t1 on t1.t_id = t0.t_id and t0.p_id =t1.p_id and t0.e_id =t1.s_id inner join teams t2 on t2.s_id =t0.t_id   where t1.DocEntry is not null  and t0.DocEntry  is not Null and t0.Sync is null"
+            Dim dt_linkSer As New DataTable
+            dt_linkSer = getDataTable(strlinkSer)
+            For Each row As DataRow In dt_linkSer.Rows
+                Try
+                    Dim DocEntry As Integer = 0
+                    Dim errors_ As New List(Of String)
+                    Dim ActivityId As String = row("ActivityId")
+                    Dim ServiceId As String = row("ServiceId")
+                    Dim taskId As String = row("s_id")
+                    'Dim cardCode As String = row("CardCode")
+                    Dim rwCnt As String = String.Format("Select Count(*) from SCL5 where SrvcCallId = {0}", ServiceId)
+                    Dim dtRw As DataTable = getDataTableSAP(rwCnt)
+                    Dim rw As Integer = dtRw.Rows(0)(0)
+                    objDiOperations.updateServiceByActivity(ActivityId, ServiceId, rw, taskId, errors_, DocEntry)
+                    If errors_.Count = 0 And DocEntry <> 0 Then
+                        Dim updateProject = String.Format("Update tasks set Sync = 'Yes' where s_id = '{0}'", taskId)
+                        ExcuteNonQuerySciNote(updateProject)
+                    End If
+                Catch ex As Exception
+
+                End Try
+            Next
+
+            writeLog(String.Format("Attachment :{0}", attPath), True)
+            objDiOperations.AddAttachmentInSAP(attPath)
+
+            Application.Exit()
+                Catch ex As Exception
+
+                End Try
+    End Sub
+
+End Module
---
+++ SciNoteIntegrator_11072024_ScinoteTOStaging (2)/SciNoteIntegrator_11072024_ScinoteTOStaging/SciNoteIntegrator/SciNoteIntegrator/clsProject.vb
@@ -0,0 +1,77 @@
+﻿
+Public Class clsProject
+    Public Property data As List(Of datap)
+
+
+    'Public teams As String
+    'Public attributes As List(Of Attribute)
+
+
+
+End Class
+
+Public Class clsProjectSNG
+    Public Property data As datap
+
+
+    'Public teams As String
+    'Public attributes As List(Of Attribute)
+
+
+
+End Class
+
+
+
+Public Class clsProject_N
+    Public Property data As List(Of dataN)
+
+
+    'Public teams As String
+    'Public attributes As List(Of Attribute)
+
+
+
+End Class
+
+Public Class dataN
+    Public Property id As Integer
+    Public Property type As String
+    ' Public Property attributes
+End Class
+Public Class datap
+    Public Property id As Integer
+    Public Property type As String
+    Public Property archived As String
+    Public Property attributes
+End Class
+Public Class attrip
+
+    'INSERT INTO projects (t_id ,s_id ,name ,visibility ,start_date ,archived ,created_at ,updated_at) VALUES ('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}') 
+
+    Public Property name
+    Public Property description
+    Public Property visibility As String
+    Public start_date As DateTime
+    Public Property archived
+
+    Public created_at As DateTime
+    Public updated_at As DateTime
+
+    'INSERT INTO dbo.tasks (t_id ,p_id ,e_id ,s_id ,name ,started_on ,due_date ,description ,state ,archived ,status_id ,status_name ,prev_status_id ,prev_status_name ,next_status_id ,next_status_name ,x ,y ,created_at ,updated_at) VALUES ('{0}','{1}' ,'{2}' ,'{3}','{4}' ,'{5}','{6}' ,'{7}' ,'{8}' ,'{9}' ,'{10}' ,'{11}' ,'{12}' ,'{13}' ,'{14}' ,'{15}' ,'{16}' ,'{17}' ,'{18}' ,'{19}')
+
+    Public Property started_on
+    Public Property due_date
+
+    Public Property state
+
+    Public Property status_id
+    Public Property status_name
+    Public Property prev_status_id
+    Public Property prev_status_name
+    Public Property next_status_id
+    Public Property next_status_name
+    Public Property x
+    Public Property y
+
+End Class
